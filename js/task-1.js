5f8c4c4cdd32ff039c231b69f557e236bcf827b9
Valeri-555 <106805806+Valeri-555@users.noreply.github.com>
2024-01-14 23:03:50 +0200

    update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,8 @@
-function makeTransaction(quantity, pricePerDroid) {
+function makeTransaction (quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 };
+makeTransaction();
 
 console.log(makeTransaction(5, 3000)); 
 console.log(makeTransaction(3, 1000)); 
